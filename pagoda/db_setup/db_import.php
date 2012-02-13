40794512929b0bde7e6ec772672fd247984e917e
Jed Galbraith <jed@delorum.com>
2012-02-13 15:48:18 -0700

    git c

--- pagoda/db_setup/db_import.php
+++ pagoda/db_setup/db_import.php
@@ -1,13 +1,13 @@
 <?php 
 
-$link = mysql_connect('localhost', 'root', 'root');
-// $link = mysql_connect($_SERVER["DB1_HOST"], $_SERVER["DB1_USER"], $_SERVER["DB1_PASS"]);
+// $link = mysql_connect('localhost', 'root', 'root');
+$link = mysql_connect($_SERVER["DB1_HOST"], $_SERVER["DB1_USER"], $_SERVER["DB1_PASS"]);
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
 
-mysql_select_db('demo-ci-install-auto'); 
-// mysql_select_db($_SERVER["DB1_NAME"]);
+// mysql_select_db('demo-ci-install-auto'); 
+mysql_select_db($_SERVER["DB1_NAME"]);
 
 // $sql = file_get_contents('/Users/jedgalbraith/Sites/framework-demos/codeigniter-install/pagoda/db_setup/ci-demo-blog-setup.sql');
 // // $sql = file_get_contents('pagoda/db_setup/test.sql');
