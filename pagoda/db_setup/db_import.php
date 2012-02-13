78cf4855eceed4c0ad8d0e2695f016a8681b032a
Jed Galbraith <jed@delorum.com>
2012-02-13 09:24:00 -0700

    removed /

--- pagoda/db_setup/db_import.php
+++ pagoda/db_setup/db_import.php
@@ -2,19 +2,16 @@
 
 <?php 
 
-print_r ($_ENV);
-print_r ($_SERVER); die;
-
 // $link = mysql_connect('localhost', 'root', 'root');
-$link = mysql_connect($_ENV["DB1_HOST"], $_ENV["DB1_USER"], $_ENV["DB1_PASS"]);
+$link = mysql_connect($_SERVER["DB1_HOST"], $_SERVER["DB1_USER"], $_SERVER["DB1_PASS"]);
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
 
-mysql_select_db($_ENV["DB1_NAME"]); 
+mysql_select_db($_SERVER["DB1_NAME"]); 
 
 // $sql = file_get_contents('/Users/jedgalbraith/Sites/framework-demos/codeigniter-install/pagoda/db_setup/test.sql');
-$sql = file_get_contents('/pagoda/db_setup/test.sql');
+$sql = file_get_contents('pagoda/db_setup/test.sql');
 
 $result = mysql_query($sql);
 if (!$result) {
