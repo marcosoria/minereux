e4a062be6fd5e833799f2b17f3ca541c37d3ee16
Jed Galbraith <jed@delorum.com>
2012-02-15 15:04:50 -0700

    removed local creds

--- pagoda/db_setup/db_import.php
+++ pagoda/db_setup/db_import.php
@@ -2,22 +2,12 @@
 
 include ('sql_parse.php');
 
-if (isset($_SERVER['PLATFORM']) && $_SERVER['PLATFORM'] == 'PAGODA') {
-  $link = mysql_connect($_SERVER["DB1_HOST"], $_SERVER["DB1_USER"], $_SERVER["DB1_PASS"]);
-  if (!$link) {
-    die('Could not connect: ' . mysql_error());
-  }
-  mysql_select_db($_SERVER["DB1_NAME"]);
-  $parseObj = new ParseSql('pagoda/db_setup/ci-demo-blog-setup.sql'); 
+$link = mysql_connect($_SERVER["DB1_HOST"], $_SERVER["DB1_USER"], $_SERVER["DB1_PASS"]);
+if (!$link) {
+  die('Could not connect: ' . mysql_error());
 }
-else {
-  $link = mysql_connect('localhost', 'root', 'root');
-  if (!$link) {
-    die('Could not connect: ' . mysql_error());
-  }
-  mysql_select_db('demo-ci-install-auto'); 
-  $parseObj = new ParseSql('ci-demo-blog-setup.sql'); 
-}
+mysql_select_db($_SERVER["DB1_NAME"]);
+$parseObj = new ParseSql('pagoda/db_setup/ci-demo-blog-setup.sql'); 
 
 echo 'sql file parse: START' . '<br />';
 
