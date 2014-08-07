be724a409f7e910c0e7367a0d2cef0be1ac4d480
Marco Soria <ingsoria@gmail.com>
2014-08-07 07:40:02 -0700

    about

--- application/views/page/about.php
+++ application/views/page/about.php
@@ -1,1 +1,135 @@
-this is about
+<!--==============================content================================-->
+<section id="content">         
+    <div class="container_12">
+        <div class="wrapper bg-wrapper-1">
+            <div class="grid_7">
+                <h3 class="bot-3">Acerca de Minereux</h3>
+                <div class="extra-wrap bot-1">
+                    <div class="grid_4 alpha">
+                        <figure class="figure-1">
+                            <img src="images/page-1-img.jpg" alt="">
+                        </figure>
+                    </div>
+                    <div class="grid_3 omega">
+                        <figure class="figure-1">
+                            <img src="images/page-1-img-1.jpg" alt="">
+                        </figure>
+                    </div>
+                </div>
+
+                <span class="font-14 col">Experience. Quality. Reliability!</span>
+                <p>Suspendisse commodo tempor sagittis! In justo est, sollicitudin eu scelerisque pretium, placerat eget elit. Praesent faucibus rutrum odio at rhoncus. Pellentesque vitae tortor id neque fermentum pretium. Maecenas ac lacus ut neque rhoncus laoreet sed id tellus. Donec justo tellus, tincidunt vitae pellentesque nec, pharetra a orci. Praesent nunc nunc, egestas eget elementum sed; rutrum eget metus! Vestibulum congue congue dui ut porta. </p>
+
+            </div>
+            <div class="grid_4 prefix_1">
+                <h3 class="bot-3">A Few Words From Our Clients</h3>
+                <div class="div-comment">
+                    <div class="comment">
+                        <p>Suspendisse commodo tempor sagittis! In justo est, sollicitudin eu scelerisque pretium, placerat eget elit. Praesent faucibus rutrum odio at rhoncus. Pellentesque vitae tortor id neque fermentum pretiuaecenas ac lacus ut neque rhoncus laoreet sed id tellus. Donec justo tellus.</p>
+                    </div>
+                    <p class="left">
+                        <strong class="dis-block"><a href="#" class="col hov">Sam Kromstain</a></strong>
+                        <span>President Customer Company</span>
+                    </p>
+                </div>
+                <a href="#" class="button">All testimonials</a>
+
+            </div>
+        </div><!--end of wrapper-->
+
+        <div class="wrapper bg-wrapper-1">
+            <div class="grid_12">
+                <h3 class="bot-3">Our Capabilities</h3>
+            </div>
+            <div class="clear"></div>
+
+            <div class="extra-wrap">
+                <div class="grid_6">
+                    <div class="wrapper-extra">
+                        <div class="letter">a</div>
+                        <div class="extra-wrap">
+                            <a href="#" class="font-14 hov-1">Liquid and solid formulation, milling, blending, fill, pack and ship to customer requirements</a>
+                            <p>Suspendisse commodo tempor sagittis! In justo est, sollicitudin eu scelerisque pretium, placerat eget elit. Praesent faucibus rutrum odio at rhoncus. Pellentesque vitae tortor id neque fermentum pretium.</p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="grid_6">
+                    <div class="wrapper-extra">
+                        <div class="letter">b</div>
+                        <div class="extra-wrap">
+                            <a href="#" class="font-14 hov-1">Rework/Repack</a>
+                            <p>In justo est, sollicitudin eu scelerisque pretium, placerat eget elit. Praesent faucibus rutrum odio at rhoncus. Pellentesque vitae tortor id neque fermentum pretium. Maecenas ac lacus ut neque rhoncus laoreet sed id tellus. Donec justo tellus, tincidunt vitae pellentesque nec.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="extra-wrap">
+                <div class="grid_6">
+                    <div class="wrapper-extra">
+                        <div class="letter">c</div>
+                        <div class="extra-wrap">
+                            <a href="#" class="font-14 hov-1">Fully equipped and staffed labs for each plant site </a>
+                            <p>Praesent faucibus rutrum odio at rhoncus. Pellentesque vitae tortor id neque fermentum pretium. Maecenas ac lacus ut neque rhoncus laoreet sed id tellus. Donec justo tellus, tincidunt vitae pellentesque nec, pharetra a orci. Praesent </p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="grid_6">
+                    <div class="wrapper-extra">
+                        <div class="letter">d</div>
+                        <div class="extra-wrap">
+                            <a href="#" class="font-14 hov-1">Custom manufacturing</a>
+                            <p>Pellentesque vitae tortor id neque fermentum pretium. Maecenas ac lacus ut neque rhoncus laoreet sed id tellus. Donec justo tellus, tincidunt vitae pellentesque nec, pharetra a orci. Praesent nunc nunc, egestas eget elementum sed; rutrum eget metus! Vestibulum congue congue dui ut porta. </p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div><!--end of wrapper-->
+
+        <div class="wrapper">
+            <div class="grid_12">
+                <h3 class="bot-3">Work team</h3>
+            </div>
+            <div class="clear"></div>
+
+            <div class="grid_3">
+                <figure class="figure-1 bot-1">
+                    <img src="images/page-1-img-2.jpg" alt="">
+                </figure>
+                <a href="#" class="font-14 hov col">Camala Haddon</a>
+                <span class="dis-block">Industrial Engineer</span>
+            </div>
+            <div class="grid_3">
+                <figure class="figure-1 bot-1">
+                    <img src="images/page-1-img-3.jpg" alt="">
+                </figure>
+                <a href="#" class="font-14 hov col">Mark Johnson</a>
+                <span class="dis-block">Strategic Designer</span>
+            </div>
+            <div class="grid_3">
+                <figure class="figure-1 bot-1">
+                    <img src="images/page-1-img-4.jpg" alt="">
+                </figure>
+                <a href="#" class="font-14 hov col">Eva Savits</a>
+                <span class="dis-block">Regional Manager</span>
+            </div>
+            <div class="grid_3">
+                <figure class="figure-1 bot-1">
+                    <img src="images/page-1-img-5.jpg" alt="">
+                </figure>
+                <a href="#" class="font-14 hov col">John Franklin</a>
+                <span class="dis-block">Director</span>
+            </div>
+
+        </div> 
+
+
+    </div>
+</section>
