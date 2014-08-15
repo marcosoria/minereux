a405d5ca43f257f2f96b97c868a6538ef8d3e7b4
Marco Soria <ingsoria@gmail.com>
2014-08-14 19:09:32 -0700

    ruta

--- application/views/page/home.php
+++ application/views/page/home.php
@@ -61,24 +61,25 @@
                     <h1><a href="index.html"><img src="/assets/images/logo.png" alt="" height="58"></a></h1>
                     <nav class="grid_12"> 
                         <ul class="sf-menu">
-                            <li class="current"><a href="/">Inicio</a></li>
-                            <li>
-                                <a href="/page/nosotros">Nosotros</a>
-                                <ul>
-                                    <li><a href="#">Historia</a></li>
-                                    <li><a href="#">Noticias</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Productos</a> 
-                                <ul>
-                                    <li><a href="/page/barita">Barita</a></li>
-                                    <li><a href="/page/hierro">Mineral de Hierro</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Servicios</a></li>
-                            <li><a href="#">Blog</a></li> 
-                            <li><a href="#">Contacto</a></li> 
-                        </ul>
+                            <li><a href="/">Inicio</a></li>
+                             <li class="<?php echo $curr_nosotros ?>">
+                             	<a href="/page/nosotros">Nosotros</a>
+                                 <ul>
+                                 	<li><a href="#">Historia</a></li>
+                                  	<li><a href="#">Noticias</a></li>
+                               	</ul>
+                               </li>
+                             <li class="<?php echo $curr_productos ?>"><a href="#">Productos</a> 
+                             	<ul>
+                             		<li><a href="/page/barita">Barita</a></li>
+                             	 	<li><a href="/page/hierro">Mineral de Hierro</a></li>
+                             	 	<li><a href="/page/arena_silica">Arena Silica</a></li>
+                             		</ul>
+                             </li>
+                             <li class="<?php echo $curr_servicios ?>"><a href="/page/ruta">Ruta</a></li>
+                             <li class="<?php echo $curr_blog ?>"><a href="#">Blog</a></li> 
+                             <li class="<?php echo $curr_contacto ?>"><a href="/page/contacto">Contacto</a></li> 
+                         </ul>
                         <div class="clear"></div>
                     </nav>
                     <div class="clear"></div>
